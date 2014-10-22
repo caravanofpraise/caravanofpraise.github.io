d65514c8c0c0f4a890cbb97ff4f0529a170419cc
wansaleh <wansaleh@gmail.com>
2014-10-22 12:16:48 +0800

    Updates.

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,1 +1,1 @@
-(function(){$(function(){return $(".backstretch-1").backstretch(""+imgurl+"/bg_brickwall.jpg")})}).call(this);
+(function(){$(function(){return new WOW({offset:100}).init(),$.backstretch([""+imgurl+"/bg_brickwall.jpg",""+imgurl+"/bg_river.jpg",""+imgurl+"/bg_canyon.jpg"],{duration:15e3,fade:1e3})})}).call(this);
